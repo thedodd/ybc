854e457f305757aab80a7bcd42aa54c2010145db
Anthony Dodd <dodd.anthonyjosiah@gmail.com>
2020-09-17 09:28:51 -0500

    Include README in docs for docs.rs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,5 @@
-//! YBC encapsulates all of the structure, style and functionality of the Bulma CSS framework
-//! as a set of Yew components. YBC also ships with support for the Yew Router, adding
-//! Bulma-styled components which wrap the Yew Router components for clean integration.
-//!
-//! As a guiding principal, YBC does not attempt to encapsulate every single Bulma style as a
-//! Rust type, let alone the many valid style combinations. That would be far too complex, and
-//! probably limiting to the user in many ways. Instead, YBC handles strucutre, required classes,
-//! functionality, sane defaults and every component can be customized with any additional
-//! classes for an exact look and feel.
-
+#![cfg_attr(feature = "docinclude", feature(external_doc))]
+#![cfg_attr(feature = "docinclude", doc(include = "../README.md"))]
 #![recursion_limit = "1024"]
 
 mod columns;
