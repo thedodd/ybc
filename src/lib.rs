458598f7ea8fd3197cc4edd88259fc0a3f5ddd4d
Anthony Dodd <dodd.anthonyjosiah@gmail.com>
2021-07-26 16:20:53 -0500

    Fix docs due to doc(include) issues
    See:
    rust-lang/rust#44732
    rust-lang/rust#82539

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,21 @@
-#![cfg_attr(feature = "docinclude", feature(external_doc))]
-#![cfg_attr(feature = "docinclude", doc(include = "../README.md"))]
+//! A [Yew](https://yew.rs) component library based on the [Bulma CSS](https://bulma.io) framework.
+//!
+//! YBC encapsulates all of the structure, style and functionality of the Bulma CSS framework as a
+//! set of Yew components. YBC also ships with support for the Yew Router, adding Bulma-styled
+//! components which wrap the Yew Router components for clean integration.
+//!
+//! As a guiding principal, YBC does not attempt to encapsulate every single Bulma style as a Rust
+//! type, let alone the many valid style combinations. That would be far too complex, and probably
+//! limiting to the user in many ways. Instead, YBC handles strucutre, required classes, functionality,
+//! sane defaults and every component can be customized with any additional classes for an exact look and feel.
+//!
+//! What does it look like to use YBC? The following is a snippet of a component's `view` method
+//! rendering a navbar, a fluid container, and some tiles.
+//!
+//! Please see [this project's README](https://github.com/thedodd/ybc) for the example. **Docs.rs is
+//! currently (2021.07.26) slightly broken and having trouble including external docs as it has historically.**
+// TODO: add this back in once it is no longer a land mine:
+//  #![cfg_attr(feature = "docinclude", doc = include_str!("../README.md"))]
 #![recursion_limit = "1024"]
 
 mod columns;
